0f8c432d1d82a2c01775ce9bb0862b7bdb62e1c4
anisioaleixo <anisioaleixo@yahoo.com.br>
2021-08-02 18:22:20 -0300

    port alter

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const routes = require('./src/routes')
 require('dotenv').config()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 app.use(routes)
